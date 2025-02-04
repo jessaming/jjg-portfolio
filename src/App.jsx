edbb8922c962d24c8f559d942db26015e9b519b1
jessaming <jessaminjhoygodio@gmail.com>
2025-02-04 13:07:08 +0800

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
 import { ReactLenis } from 'lenis/react';
 import Navbar from './components/Navbar';
 import Background from './components/Background';
@@ -9,6 +8,7 @@ import Skills from './components/Skills';
 import Works from './components/Works';
 import Connect from './components/Connect';
 import { skills } from './constants';
+import './index.css';
 
 const App = () => {
   return (
@@ -17,13 +17,11 @@ const App = () => {
         <Background />
         <Navbar />
         <div className='relative'>
-          <Cursor />
-          <Routes>
-            <Route path="/" element={<Bio id='discover' />} />
-            <Route path="/skills" element={<Skills tags={skills} id='skills' />} />
-            <Route path="/works" element={<Works id='works' />} />
-            <Route path="/connect" element={<Connect />} />
-          </Routes>
+        <Cursor/>
+          <Bio id='discover'/>
+          <Skills  tags={skills} id='skills' />
+          <Works id='works' />
+          <Connect />
         </div>
       </main>
     </ReactLenis>
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,11 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App.jsx';
 
 const root = createRoot(document.getElementById('root'));
 
 root.render(
   <StrictMode>
-    <Router basename={import.meta.env.BASE_URL}>
-      <Routes>
-        <Route path="*" element={<App />}/>
-      </Routes>
-    </Router>
+    <App />
   </StrictMode>
 );
